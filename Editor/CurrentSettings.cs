639b8f0c972d2c95aaccb04a137f1ca6b20a8855
Sean Stolberg <seans@unity3d.com>
2019-08-28 14:02:29 -0700

    Initial commit of CurrentSettings as a package

---
+++ Editor/CurrentSettings.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace com.unity.xr.test.runtimesettings
+{
+    public class CurrentSettings : ScriptableObject
+    {
+        public string EnabledXrTarget;
+        public string PlayerGraphicsApi;
+        public string StereoRenderingMode;
+
+        // OculusSettings.StereoRenderingMode, to string
+        public string StereoRenderingModeDesktop;
+        public string StereoRenderingModeAndroid;
+
+        public bool MtRendering;
+        public bool GraphicsJobs;
+
+        public string SimulationMode;
+    }
+}
